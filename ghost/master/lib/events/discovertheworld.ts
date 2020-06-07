148a6839f14d564a89cfb687a1fa55f24e387ff2
Kenta <kenta.job09@gmail.com>
2020-06-08 00:58:47 +0900

    [Update]トレンド取得をトップ10に絞った

--- ghost/master/lib/events/discovertheworld.ts
+++ ghost/master/lib/events/discovertheworld.ts
@@ -5,10 +5,15 @@ import {r} from "../utils";
 
 events.GetTwitterTrends = async () => {
   try {
+    //  トレンドトップ(いれた数字)
+    const trendsTopRank = 10;
     const trends: any = await TwitterService.getJapaneseTwitterTrends();
-    const trendsNameList = trends[0].trends.map((trend: { name: string; }) => `${trend.name}\\n`).join("");
+    const topTrends = trends[0].trends
+        .map((trend: { name: string; }) => `${trend.name}\\n`)
+        .slice(0, trendsTopRank - 1)
+        .join("");
     return r`\s[5]探してきたので読み上げますね。\w5\n` +
-           r`${trendsNameList}` +
+           r`${topTrends}` +
            r`\w5\s[1]こんな感じでよかったですか？`;
   } catch (e) {
     return r`${e}`;
